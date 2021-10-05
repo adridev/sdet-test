329a2bccfb30a6ef98c16ef0af06cedccd7efaea
adrian.rios <adrian.rios@docomodigital.com>
2021-10-05 06:36:34 +0200

    first commit

---
+++ src/main/java/com/docomodigital/sdetct/SdetCtApplication.java
@@ -0,0 +1,12 @@
+package com.docomodigital.sdetct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdetCtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdetCtApplication.class, args);
+    }
+}
---
+++ src/test/java/com/docomodigital/sdetct/SdetCtApplicationTests.java
@@ -0,0 +1,12 @@
+package com.docomodigital.sdetct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdetCtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
